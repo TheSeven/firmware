e9a96a4bf314772ad0fc11d0fa8c23a87eaa24f9
Michael Sparmann <theseven@gmx.net>
2014-12-02 23:52:08 +0100

    Fix STM32 CRC32 hardware acceleration. This will only work for word-aligned buffers.

--- src/soc/stm32/crc32.cpp
+++ src/soc/stm32/crc32.cpp
@@ -7,18 +7,16 @@
 
 uint32_t crc32(const void* buf, uint32_t len)
 {
-    uint8_t* data = (uint8_t*)buf;
+    uint32_t* data = (uint32_t*)buf;
     bool old = clockgate_enable(STM32_CRC_CLOCKGATE, true);
     union STM32_CRC_REG_TYPE::CR CR = { 0 };
     CR.b.RESET = true;
     STM32_CRC_REGS.CR.d32 = CR.d32;
-    while (len >= 4)
+    while (len >= 1)
     {
-        STM32_CRC_REGS.DR = *((uint32_t*)data);
-        data += 4;
+        STM32_CRC_REGS.DR = *data++;
         len -= 4;
     }
-    while (len--) STM32_CRC_REGS.DR = *data++;
     uint32_t crc = STM32_CRC_REGS.DR;
     clockgate_enable(STM32_CRC_CLOCKGATE, old);
     return crc;
