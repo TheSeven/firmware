94a78503efd2168dfd9de4bbdd59de2e5d7879db
Michael Sparmann <theseven@gmx.net>
2014-12-02 21:17:03 +0100

    Add StoragePartition class:
    This class emulates a smaller storage device on top of a specified
    erase block range of an underlying, bigger device.

--- src/interface/storage/storage.h
+++ src/interface/storage/storage.h
@@ -1,10 +1,17 @@
 #pragma once
 
 #include "global.h"
+#include "sys/util.h"
 
 
 class __attribute__((packed,aligned(4))) Storage
 {
+protected:
+    constexpr Storage() : pageCount(0), pageSize(0), eraseSize(0), programSize(0) {}
+
+    constexpr Storage(uint32_t pageCount, uint32_t pageSize, int eraseSize, int programSize)
+        : pageCount(pageCount), pageSize(pageSize), eraseSize(eraseSize), programSize(programSize) {}
+
 public:
     enum Result
     {
@@ -29,4 +36,46 @@ public:
     virtual enum Result read(uint32_t page, uint32_t len, void* buf) = 0;
     virtual enum Result write(uint32_t page, uint32_t len, const void* buf) = 0;
     virtual enum Result erase(uint32_t page, uint32_t len) = 0;
+};
+
+class __attribute__((packed,aligned(4))) StoragePartition final : public Storage
+{
+public:
+    Storage* storage;
+    uint32_t offset;
+
+    constexpr StoragePartition() : Storage(), storage(NULL), offset(0) {}
+
+    constexpr StoragePartition(Storage* storage, uint32_t offset, uint32_t size)
+        : Storage(size * MAX(storage->programSize, storage->eraseSize), storage->pageSize,
+                  storage->eraseSize, storage->programSize),
+          storage(storage), offset(MAX(storage->programSize, storage->eraseSize) * offset) {}
+
+    enum Result reset()
+    {
+        return storage->reset();
+    }
+
+    enum Result getStatus()
+    {
+        return storage->getStatus();
+    }
+
+    enum Result read(uint32_t page, uint32_t len, void* buf)
+    {
+        if (page >= pageCount || len > pageCount - page) return RESULT_INVALID_ARGUMENT;
+        return storage->read(page + offset, len, buf);
+    }
+
+    enum Result write(uint32_t page, uint32_t len, const void* buf)
+    {
+        if (page >= pageCount || len > pageCount - page) return RESULT_INVALID_ARGUMENT;
+        return storage->write(page + offset, len, buf);
+    }
+
+    enum Result erase(uint32_t page, uint32_t len)
+    {
+        if (page >= pageCount || len > pageCount - page) return RESULT_INVALID_ARGUMENT;
+        return storage->erase(page + offset, len);
+    }
 };
