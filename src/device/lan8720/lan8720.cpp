30b8d8637cc6551e58fbb5b16d5e2c71089ad7bb
Michael Sparmann <theseven@gmx.net>
2014-12-27 00:23:27 +0100

    LAN8720: Handle MDIO errors during sense operation properly

--- src/device/lan8720/lan8720.cpp
+++ src/device/lan8720/lan8720.cpp
@@ -19,19 +19,20 @@ enum Ethernet::Result LAN8720::power(bool on)
 
 enum Ethernet::Result LAN8720::sense(Ethernet::Interface* intf, Ethernet::MII::Driver* driver)
 {
+    Ethernet::Result rc;
     uint16_t data;
-    mac->phyRead(phyId, 0x1b, &data);
+    if ((rc = mac->phyRead(phyId, 0x1b, &data)) != Ethernet::RESULT_OK) return rc;
     enum Ethernet::MII::Wiring wiring = (data & 0x2000) ? Ethernet::MII::WIRING_MDIX : Ethernet::MII::WIRING_MDI;
     if (wiring != driver->wiring) intf->stateChanged = true;
     driver->wiring = wiring;
-    mac->phyRead(phyId, 0x1f, &data);
+    if ((rc = mac->phyRead(phyId, 0x1f, &data)) != Ethernet::RESULT_OK) return rc;
     enum Ethernet::MII::Duplex duplex = (data & 0x0010) ? Ethernet::MII::DUPLEX_FDX : Ethernet::MII::DUPLEX_HDX;
     if (duplex != driver->duplex) intf->stateChanged = true;
     driver->duplex = duplex;
     enum Ethernet::MII::Speed speed = (data & 0x0004) ? Ethernet::MII::SPEED_10 : Ethernet::MII::SPEED_100;
     if (speed != driver->speed) intf->stateChanged = true;
     driver->speed = speed;
-    mac->phyRead(phyId, 0x01, &data);
+    if ((rc = mac->phyRead(phyId, 0x01, &data)) != Ethernet::RESULT_OK) return rc;
     bool linkUp = !!(data & 0x0004);
     if (linkUp != intf->linkUp) intf->stateChanged = true;
     intf->linkUp = linkUp;
