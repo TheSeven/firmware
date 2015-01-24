9eb856b38733f27735d09bb6a099c1407d21490f
Michael Sparmann <theseven@gmx.net>
2015-01-25 00:53:31 +0100

    ArtNetNode: Adjust some buffer sizes, and use inofficial memory for execute from RAM builds This fixes an issue with DHCP not working on F205 RAM builds.

--- src/target/artnetnode/flexperiment/mini/f205/ram/target.h
+++ src/target/artnetnode/flexperiment/mini/f205/ram/target.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include "../target.h"
-
+#define STM32_USE_INOFFICIAL
 #define STM32_ETHMAC_BUFFER_SIZE 256
-#define STM32_ETHMAC_NUM_RX_BUFS 16
-#define STM32_ETHMAC_NUM_TX_BUFS 8
+#define STM32_ETHMAC_NUM_RX_BUFS 6
+#define STM32_ETHMAC_NUM_TX_BUFS 6
+
+#include "../target.h"
