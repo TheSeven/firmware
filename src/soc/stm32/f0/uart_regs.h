463ba1fe7404df406efd6d5a17b54341c0cd1fef
Michael Sparmann <theseven@gmx.net>
2015-01-31 12:30:55 +0100

    STM32: Add (untested) support for UART on F0 series chips

---
+++ src/soc/stm32/f0/uart_regs.h
@@ -0,0 +1,180 @@
+#pragma once
+
+struct __attribute__((packed)) STM32_USART_REG_TYPE
+{
+    union CR1
+    {
+        uint32_t d32;
+        struct __attribute__((packed)) b
+        {
+            uint32_t UE : 1;
+            uint32_t : 1;
+            uint32_t RE : 1;
+            uint32_t TE : 1;
+            uint32_t IDLEIE : 1;
+            uint32_t RXNEIE : 1;
+            uint32_t TCIE : 1;
+            uint32_t TXEIE : 1;
+            uint32_t PEIE : 1;
+            uint32_t PS : 1;
+            uint32_t PCE : 1;
+            uint32_t WAKE : 1;
+            uint32_t M0 : 1;
+            uint32_t MME : 1;
+            uint32_t CMIE : 1;
+            uint32_t OVER8 : 1;
+            uint32_t DEDT : 5;
+            uint32_t DEAT : 5;
+            uint32_t RTOIE : 1;
+            uint32_t EOBIE : 1;
+            uint32_t M1 : 1;
+            uint32_t : 3;
+        } b;
+    } CR1;
+    union CR2
+    {
+        uint32_t d32;
+        struct __attribute__((packed)) b
+        {
+            uint32_t : 4;
+            uint32_t ADDM7 : 1;
+            uint32_t : 3;
+            uint32_t LBCL : 1;
+            uint32_t CPHA : 1;
+            uint32_t CPOL : 1;
+            uint32_t CLKEN : 1;
+            uint32_t STOP : 2;
+            uint32_t : 1;
+            uint32_t SWAP : 1;
+            uint32_t RXINV : 1;
+            uint32_t TXINV : 1;
+            uint32_t DATAINV : 1;
+            uint32_t MSBFIRST : 1;
+            uint32_t ABREN : 1;
+            uint32_t ABRMOD : 2;
+            uint32_t RTOEN : 1;
+            uint32_t ADD : 8;
+        } b;
+    } CR2;
+    union CR3
+    {
+        uint32_t d32;
+        struct __attribute__((packed)) b
+        {
+            uint32_t EIE : 1;
+            uint32_t : 2;
+            uint32_t HDSEL : 1;
+            uint32_t : 2;
+            uint32_t DMAR : 1;
+            uint32_t DMAT : 1;
+            uint32_t RTSE : 1;
+            uint32_t CTSE : 1;
+            uint32_t CTSIE : 1;
+            uint32_t ONEBIT : 1;
+            uint32_t OVRDIS : 1;
+            uint32_t DDRE : 1;
+            uint32_t DEM : 1;
+            uint32_t DEP : 1;
+            uint32_t : 16;
+        } b;
+    } CR3;
+    union BRR
+    {
+        uint32_t d32;
+        struct __attribute__((packed)) b
+        {
+            uint32_t DIV_Fraction : 4;
+            uint32_t DIV_Mantissa : 12;
+            uint32_t : 16;
+        } b;
+    } BRR;
+    union GTPR
+    {
+        uint32_t d32;
+        struct __attribute__((packed)) b
+        {
+            uint32_t : 32;
+        } b;
+    } GTPR;
+    uint32_t RTOR;
+    union RQR
+    {
+        uint32_t d32;
+        struct __attribute__((packed)) b
+        {
+            uint32_t ABRRQ : 1;
+            uint32_t SBKRQ : 1;
+            uint32_t MMRQ : 1;
+            uint32_t RXFRQ : 1;
+            uint32_t : 32;
+        } b;
+    } RQR;
+    union ISR
+    {
+        uint32_t d32;
+        struct __attribute__((packed)) b
+        {
+            uint32_t PE : 1;
+            uint32_t FE : 1;
+            uint32_t NF : 1;
+            uint32_t ORE : 1;
+            uint32_t IDLE : 1;
+            uint32_t RXNE : 1;
+            uint32_t TC : 1;
+            uint32_t TXE : 1;
+            uint32_t : 1;
+            uint32_t CTSIF : 1;
+            uint32_t CTS : 1;
+            uint32_t RTOF : 1;
+            uint32_t : 2;
+            uint32_t ABRE : 1;
+            uint32_t ABRF : 1;
+            uint32_t BUSY : 1;
+            uint32_t CMF : 1;
+            uint32_t SBKF : 1;
+            uint32_t : 2;
+            uint32_t TEACK : 1;
+            uint32_t : 10;
+        } b;
+    } ISR;
+    union ICR
+    {
+        uint32_t d32;
+        struct __attribute__((packed)) b
+        {
+            uint32_t PE : 1;
+            uint32_t FE : 1;
+            uint32_t NF : 1;
+            uint32_t ORE : 1;
+            uint32_t IDLE : 1;
+            uint32_t : 1;
+            uint32_t TC : 1;
+            uint32_t : 2;
+            uint32_t CTS : 1;
+            uint32_t : 1;
+            uint32_t RTOF : 1;
+            uint32_t : 5;
+            uint32_t CMF : 1;
+            uint32_t : 14;
+        } b;
+    } ICR;
+    uint32_t RDR;
+    uint32_t TDR;
+};
+
+#ifdef SOC_STM32F1
+#define STM32_USART1_REGS (*((volatile STM32_USART_REG_TYPE*)0x40013800))
+#define STM32_USART2_REGS (*((volatile STM32_USART_REG_TYPE*)0x40004400))
+#define STM32_USART3_REGS (*((volatile STM32_USART_REG_TYPE*)0x40004800))
+#define STM32_UART4_REGS (*((volatile STM32_USART_REG_TYPE*)0x40004c00))
+#define STM32_UART5_REGS (*((volatile STM32_USART_REG_TYPE*)0x40005000))
+#else
+#define STM32_USART1_REGS (*((volatile STM32_USART_REG_TYPE*)0x40011000))
+#define STM32_USART2_REGS (*((volatile STM32_USART_REG_TYPE*)0x40004400))
+#define STM32_USART3_REGS (*((volatile STM32_USART_REG_TYPE*)0x40004800))
+#define STM32_UART4_REGS (*((volatile STM32_USART_REG_TYPE*)0x40004c00))
+#define STM32_UART5_REGS (*((volatile STM32_USART_REG_TYPE*)0x40005000))
+#define STM32_USART6_REGS (*((volatile STM32_USART_REG_TYPE*)0x40011400))
+#define STM32_UART7_REGS (*((volatile STM32_USART_REG_TYPE*)0x40007800))
+#define STM32_UART8_REGS (*((volatile STM32_USART_REG_TYPE*)0x40007c00))
+#endif
--- src/soc/stm32/uart.cpp
+++ src/soc/stm32/uart.cpp
@@ -1,16 +1,18 @@
 #include "global.h"
 #include "soc/stm32/uart.h"
-#include "soc/stm32/uart_regs.h"
 #include "cpu/arm/cortexm/irq.h"
 #include "interface/irq/irq.h"
 #include "interface/clockgate/clockgate.h"
 #include "sys/util.h"
 
 #if defined(SOC_STM32F0)
+#include "soc/stm32/f0/uart_regs.h"
 #include "soc/stm32/f0/rcc.h"
 #elif defined(SOC_STM32F1)
+#include "soc/stm32/uart_regs.h"
 #include "soc/stm32/f1/rcc.h"
 #elif defined(SOC_STM32F2) || defined(SOC_STM32F4)
+#include "soc/stm32/uart_regs.h"
 #include "soc/stm32/f2/rcc.h"
 #endif
 
@@ -22,9 +24,11 @@ namespace STM32
     {
         &STM32_USART1_REGS,
         &STM32_USART2_REGS,
+#if defined(SOC_STM32F1) || defined(SOC_STM32F2) || defined(SOC_STM32F4)
         &STM32_USART3_REGS,
         &STM32_UART4_REGS,
         &STM32_UART5_REGS,
+#endif
 #if defined(SOC_STM32F2) || defined(SOC_STM32F4)
         &STM32_USART6_REGS,
 #endif
@@ -38,9 +42,11 @@ namespace STM32
     {
         STM32_USART1_CLOCKGATE,
         STM32_USART2_CLOCKGATE,
+#if defined(SOC_STM32F1) || defined(SOC_STM32F2) || defined(SOC_STM32F4)
         STM32_USART3_CLOCKGATE,
         STM32_UART4_CLOCKGATE,
         STM32_UART5_CLOCKGATE,
+#endif
 #if defined(SOC_STM32F2) || defined(SOC_STM32F4)
         STM32_USART6_CLOCKGATE,
 #endif
@@ -54,9 +60,11 @@ namespace STM32
     {
         usart1_IRQn,
         usart2_IRQn,
+#if defined(SOC_STM32F1) || defined(SOC_STM32F2) || defined(SOC_STM32F4)
         usart3_IRQn,
         uart4_IRQn,
         uart5_IRQn,
+#endif
 #if defined(SOC_STM32F2) || defined(SOC_STM32F4)
         usart6_IRQn,
 #endif
@@ -66,6 +74,7 @@ namespace STM32
 #endif
     };
 
+#if defined(SOC_STM32F1) || defined(SOC_STM32F2) || defined(SOC_STM32F4)
     RCC::APBBus uart_buses[] =
     {
         RCC::APB2,
@@ -81,11 +90,16 @@ namespace STM32
         RCC::APB1,
 #endif
     };
+#endif
 
     enum ::UART::Result UART::configure(int baudrate, int tolerance, Config config)
     {
         volatile STM32_USART_REG_TYPE* regs = uart_regs[index];
+#if defined(SOC_STM32F1) || defined(SOC_STM32F2) || defined(SOC_STM32F4)
         int baseclock = RCC::getAPBClockFrequency(uart_buses[index]);
+#else
+        int baseclock = RCC::getAPBClockFrequency();
+#endif
         int divisor = (baseclock * 2 / baudrate + 1) / 2;
         if (divisor < 8) divisor = 8;
         int actualrate = baseclock / divisor;
@@ -123,7 +137,11 @@ namespace STM32
     void UART::bwTx(uint8_t byte)
     {
         volatile STM32_USART_REG_TYPE* regs = uart_regs[index];
+#if defined(SOC_STM32F1) || defined(SOC_STM32F2) || defined(SOC_STM32F4)
         while (!regs->SR.b.TXE);
+#else
+        while (!regs->ISR.b.TXE);
+#endif
         union STM32_USART_REG_TYPE::CR1 CR1 = { regs->CR1.d32 };
         CR1.b.TXEIE = true;
         regs->CR1.d32 = CR1.d32;;
@@ -133,13 +151,21 @@ namespace STM32
     void UART::flush()
     {
         volatile STM32_USART_REG_TYPE* regs = uart_regs[index];
+#if defined(SOC_STM32F1) || defined(SOC_STM32F2) || defined(SOC_STM32F4)
         while (!regs->SR.b.TC);
+#else
+        while (!regs->ISR.b.TC);
+#endif
     }
 
     void UART::irq()
     {
         volatile STM32_USART_REG_TYPE* regs = uart_regs[index];
+#if defined(SOC_STM32F1) || defined(SOC_STM32F2) || defined(SOC_STM32F4)
         union STM32_USART_REG_TYPE::SR SR = { regs->SR.d32 };
+#else
+        union STM32_USART_REG_TYPE::ISR SR = { regs->ISR.d32 };
+#endif
         if (SR.b.RXNE)
         {
             if (SR.b.ORE) bwRxCallback(0, ::UART::RESULT_OVERFLOW_ERROR);
@@ -147,7 +173,11 @@ namespace STM32
             ::UART::Result result = ::UART::RESULT_OK;
             if (SR.b.FE) result = ::UART::RESULT_FRAMING_ERROR;
             else if (SR.b.PE) result = ::UART::RESULT_PARITY_ERROR;
+#if defined(SOC_STM32F1) || defined(SOC_STM32F2) || defined(SOC_STM32F4)
             else if (SR.b.NE) result = ::UART::RESULT_NOISE_ERROR;
+#else
+            else if (SR.b.NF) result = ::UART::RESULT_NOISE_ERROR;
+#endif
             bwRxCallback(data, result);
         }
         if (SR.b.TXE)
@@ -159,14 +189,20 @@ namespace STM32
         }
     }
 
-    UART UART::UART1 = UART(0);
-    UART UART::UART2 = UART(1);
-    UART UART::UART3 = UART(2);
-    UART UART::UART4 = UART(3);
-    UART UART::UART5 = UART(4);
-    UART UART::UART6 = UART(5);
-    UART UART::UART7 = UART(6);
-    UART UART::UART8 = UART(7);
+    UART UART::UART1(0);
+    UART UART::UART2(1);
+#if defined(SOC_STM32F1) || defined(SOC_STM32F2) || defined(SOC_STM32F4)
+    UART UART::UART3(2);
+    UART UART::UART4(3);
+    UART UART::UART5(4);
+#endif
+#if defined(SOC_STM32F2) || defined(SOC_STM32F4)
+    UART UART::UART6(5);
+#endif
+#ifdef SOC_STM32F42X
+    UART UART::UART7(6);
+    UART UART::UART8(7);
+#endif
 }
 
 extern "C" void usart1_irqhandler()
@@ -179,6 +215,7 @@ extern "C" void usart2_irqhandler()
     STM32::UART::UART2.irq();
 }
 
+#if defined(SOC_STM32F1) || defined(SOC_STM32F2) || defined(SOC_STM32F4)
 extern "C" void usart3_irqhandler()
 {
     STM32::UART::UART3.irq();
@@ -193,12 +230,16 @@ extern "C" void uart5_irqhandler()
 {
     STM32::UART::UART5.irq();
 }
+#endif
 
+#if defined(SOC_STM32F2) || defined(SOC_STM32F4)
 extern "C" void usart6_irqhandler()
 {
     STM32::UART::UART6.irq();
 }
+#endif
 
+#ifdef SOC_STM32F42X
 extern "C" void uart7_irqhandler()
 {
     STM32::UART::UART7.irq();
@@ -208,3 +249,4 @@ extern "C" void uart8_irqhandler()
 {
     STM32::UART::UART8.irq();
 }
+#endif
