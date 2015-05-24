4b881c2788656d51c419014ea5b89269ce90ec56
Michael Sparmann <theseven@gmx.net>
2015-05-24 19:13:43 +0200

    UART: Fix broken Buffered template, add some new functions

--- src/interface/uart/uart.h
+++ src/interface/uart/uart.h
@@ -89,7 +89,7 @@ namespace UART
         }
     };
 
-    class __attribute__((packed,aligned(4))) SoftwareDMA : public DMA, BytewiseHandler
+    class __attribute__((packed,aligned(4))) SoftwareDMA : public DMA, public BytewiseHandler
     {
     protected:
         const uint8_t* volatile txBuf;
@@ -98,6 +98,8 @@ namespace UART
         uint16_t volatile rxLen;
 
     public:
+        SoftwareDMA(Bytewise* uart) : DMA(), BytewiseHandler(uart) {}
+
         void dmaTx(const uint8_t* data, int len);
         void cancelTx();
         void bwTxCallback();
@@ -106,26 +108,162 @@ namespace UART
         void bwRxCallback(uint8_t byte, enum Result status);
     };
 
-    template<typename RXBuf, typename TXBuf> class __attribute__((packed,aligned(4))) Buffered : public BytewiseHandler
+    template<size_t RXBuf, size_t TXBuf> class __attribute__((packed,aligned(4))) Buffered;
+
+    template<size_t RXBuf, size_t TXBuf> class __attribute__((packed,aligned(4))) BufferedHandler
     {
     protected:
-        RingBuffer<TXBuf> txBuf;
-        RingBuffer<RXBuf> rxBuf;
+        Buffered<RXBuf, TXBuf>* uart;
+
+        BufferedHandler(Buffered<RXBuf, TXBuf>* uart) : uart(uart)
+        {
+            uart->setHandler(this);
+        }
 
     public:
-        int readAvailable();
-        int writeAvailable();
-        bool write(uint8_t byte);
-        bool read(uint8_t* byte);
-        int write(const uint8_t* buf, int len);
-        int read(uint8_t* buf, int len);
-        void blockingWrite(uint8_t byte);
-        void blockingRead(uint8_t* byte);
-        void blockingWrite(const uint8_t* buf, int len);
-        void blockingRead(uint8_t* buf, int len);
-        void bwTxCallback();
-        void bwRxCallback(uint8_t byte, enum Result status);
         virtual void rxErrorCallback(int bytesBefore, enum Result status) = 0;
+    };
+
+    template<size_t RXBuf, size_t TXBuf> class __attribute__((packed,aligned(4))) Buffered : public BytewiseHandler
+    {
+    protected:
+        RingBuffer<uint8_t, TXBuf> txBuf;
+        RingBuffer<uint8_t, RXBuf> rxBuf;
+        BufferedHandler<RXBuf, TXBuf>* handler;
+
+    public:
+        Buffered(Bytewise* uart) : BytewiseHandler(uart), handler(NULL) {}
+
+        void setHandler(BufferedHandler<RXBuf, TXBuf>* handler)
+        {
+            this->handler = handler;
+        }
+
+        int readAvailable()
+        {
+            return rxBuf.used();
+        }
+
+        int writeAvailable()
+        {
+            return txBuf.free();
+        }
+
+        uint8_t peek()
+        {
+            uint8_t data;
+            rxBuf.peek(&data);
+            return data;
+        }
+
+        bool write(uint8_t byte)
+        {
+            enter_critical_section();
+            bool empty = !txBuf.used();
+            bool result = txBuf.put(&byte);
+            leave_critical_section();
+            if (result && empty) bwTxCallback();
+            return result;
+        }
+
+        uint8_t read()
+        {
+            uint8_t byte;
+            rxBuf.get(&byte);
+            return byte;
+        }
+
+        bool read(uint8_t* byte)
+        {
+            return rxBuf.get(byte);
+        }
+
+        int write(const uint8_t* buf, int len)
+        {
+            enter_critical_section();
+            bool empty = !txBuf.used();
+            bool result = txBuf.put(buf, len);
+            leave_critical_section();
+            if (result && empty) bwTxCallback();
+            return result;
+        }
+
+        int read(uint8_t* buf, int len)
+        {
+            return rxBuf.get(buf, len);
+        }
+
+        void blockingWrite(uint8_t byte)
+        {
+            while (true)
+            {
+                enter_critical_section();
+                bool empty = !txBuf.used();
+                bool result = txBuf.write(&byte);
+                if (!result) idle();
+                leave_critical_section();
+                if (result && empty) bwTxCallback();
+                if (result) break;
+            }
+        }
+
+        void blockingRead(uint8_t* byte)
+        {
+            while (true)
+            {
+                if (rxBuf.read(byte)) return;
+                enter_critical_section();
+                if (!rxBuf.used()) idle();
+                leave_critical_section();
+            }
+        }
+
+        void blockingWrite(const uint8_t* buf, int len)
+        {
+            while (len)
+            {
+                enter_critical_section();
+                bool empty = !txBuf.used();
+                int written = txBuf.put(buf, len);
+                if (!written) idle();
+                leave_critical_section();
+                if (written && empty) bwTxCallback();
+                len -= written;
+                if (!len) return;
+                buf += written;
+            }
+        }
+
+        void blockingRead(uint8_t* buf, int len)
+        {
+            while (len)
+            {
+                int read = rxBuf.read(buf, len);
+                len -= read;
+                if (!len) return;
+                buf += read;
+                enter_critical_section();
+                if (!rxBuf.used()) idle();
+                leave_critical_section();
+            }
+        }
+
+        void bwTxCallback()
+        {
+            uint8_t byte;
+            if (txBuf.get(&byte)) uart->bwTx(byte);
+        }
+
+        void bwRxCallback(uint8_t byte, enum Result status)
+        {
+            if (status == RESULT_OK && !rxBuf.put(&byte)) status = RESULT_OVERFLOW_ERROR;
+            if (status != RESULT_OK) rxErrorCallback(rxBuf.used(), status);
+        }
+
+        void rxErrorCallback(int bytesBefore, enum Result status)
+        {
+            if (handler) handler->rxErrorCallback(bytesBefore, status);
+        }
     };
 
 }
