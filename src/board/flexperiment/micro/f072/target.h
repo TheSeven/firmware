4e30feb31215efd98103ebe5e294145bc56cbe46
Michael Sparmann <theseven@gmx.net>
2015-11-05 20:56:07 +0100

    FLEXperiment: Add basic support for micro series

---
+++ src/board/flexperiment/micro/f072/target.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define BOARD_FLEXPERIMENT_MICRO
+#define SOC_STM32F072
+#define SOC_STM32F0XXX8
+#define STM32_VOLTAGE 3300
+#define STM32_CLOCKSOURCE_HSI48
+#define STM32_NO_PLL
+#include "soc/stm32/f0/target.h"
---
+++ src/board/flexperiment/mini/f042/target.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define BOARD_FLEXPERIMENT_MINI
+#define SOC_STM32F030
+#define SOC_STM32F0XXX4
+#define STM32_VOLTAGE 3300
+#define STM32_CLOCKSOURCE_HSI48
+#define STM32_NO_PLL
+#include "soc/stm32/f0/target.h"
