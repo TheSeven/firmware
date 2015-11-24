37e1cace2cfde72a572fbaca9062b5609f21bd48
Michael Sparmann <theseven@gmx.net>
2015-11-24 19:58:08 +0100

    printf: Whitespace fixes

--- src/lib/printf/printf.h
+++ src/lib/printf/printf.h
@@ -13,9 +13,9 @@
 extern "C"
 {
 #endif
-    extern int printf_format(int (*push)(void *userp, unsigned char data), void *userp, const char *fmt, va_list args);
-    extern int vsnprintf(char *buf, size_t size, const char *fmt, va_list ap);
-    extern int snprintf(char *buf, size_t size, const char *fmt, ...);
+    extern int printf_format(int (*push)(void* userp, unsigned char data), void* userp, const char* fmt, va_list args);
+    extern int vsnprintf(char* buf, size_t size, const char* fmt, va_list ap);
+    extern int snprintf(char* buf, size_t size, const char* fmt, ...);
 #ifdef __cplusplus
 }
 #endif
