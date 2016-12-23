31bf6ffaf6daf64a296d7f83ebbedad3619f9d6a
Michael Sparmann <theseven@gmx.net>
2016-12-24 00:36:16 +0100

    STM32: Add MOVE_CODE_INTO_PLACE, more cautious IRQ enabling during init, to support reboot without reset, e.g. for firmware updating

--- src/soc/stm32/init.cpp
+++ src/soc/stm32/init.cpp
@@ -23,21 +23,18 @@ using namespace STM32;
 
 
 // These aren't variables, but actually just symbols. Declare them as bytes to make pointer arithmetic work.
-extern "C" uint8_t _data, _data_end, _data_src, _bss, _bss_end, _dmabss, _dmabss_end;
+extern "C" uint8_t _text, _text_end, _data, _data_end, _data_src, _bss, _bss_end, _dmabss, _dmabss_end;
 
 
 void __attribute__((noreturn)) init_stage2();
 
 // This is the reset vector entry point
-void __attribute__((noreturn)) init()
+void __attribute__((weak,noreturn)) init()
 {
 #ifndef CPU_ARM_CORTEX_M0
     // Set up IRQ vector table location
     SCB->VTOR = (uint32_t)_irqvectors;
 #endif
-
-    // Enable IRQs
-    leave_critical_section();
 
     // Initialize system clocking
     RCC::init();
@@ -47,6 +44,13 @@ void __attribute__((noreturn)) init()
     SYSCFG::init();
 #endif
     
+#ifdef MOVE_CODE_INTO_PLACE
+    // Copy .text and .data sections to their destination addresses
+    void* origin, tmp;
+    asm("get_origin: ADR %0, get_origin; LDR %1, =_text - get_origin; ADD %0, %0, %1" : "=l"(origin), "=l"(tmp));
+    memmove(&_text, origin, &_data_end - &_text);
+#endif
+
     // Jump to stage 2 at the correct address
     __attribute__((noreturn)) void (*stage2)() = (TYPE_NORETURN(*)())(((uint32_t)init_stage2) | 1);
     stage2();
@@ -81,6 +85,12 @@ void __attribute__((noreturn)) init_stage2()
 #else
     STM32::GPIO::init();
 #endif
+
+    // Disable all IRQ sources
+    for (uint32_t i = 0; i < ARRAYLEN(NVIC->ICER); i++) NVIC->ICER[i] = 0xffffffff;
+
+    // Enable IRQs
+    leave_critical_section();
 
     // Initialize the system tick IRQ and microsecond timer
     time_init();
