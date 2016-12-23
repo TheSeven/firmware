4609ae0e3dc6e8d070c94bf549af1b47b44f38d9
Michael Sparmann <theseven@gmx.net>
2016-12-23 23:39:43 +0100

    STM32 EXTI: Fix base address for F2/F4 series

--- src/soc/stm32/exti_regs.h
+++ src/soc/stm32/exti_regs.h
@@ -10,4 +10,8 @@ struct __attribute__((packed)) STM32_EXTI_REG_TYPE
     uint32_t PR;
 };
 
+#if defined(SOC_STM32F0) || defined(SOC_STM32F1)
 #define STM32_EXTI_REGS (*((volatile STM32_EXTI_REG_TYPE*)0x40010400))
+#else
+#define STM32_EXTI_REGS (*((volatile STM32_EXTI_REG_TYPE*)0x40013c00))
+#endif
